ab77149acff9cbc5c89212fb6caf88ad850a0595
moritzdemmer <56080577+moritzdemmer@users.noreply.github.com>
2019-10-29 16:39:47 +0100

    Add files via upload

--- Controller/Admin/VoucherSerieGroups.php
+++ Controller/Admin/VoucherSerieGroups.php
@@ -36,24 +36,17 @@ class VoucherSerieGroups extends VoucherSerieGroups_parent
     public function getBirthdayChildsGroup()
     {
 
-        $today_date = getdate();
-        if ($today_date["mon"] < 10)
-            $birthmonth = "0" . $today_date["mon"];
-        else
-            $birthmonth = $today_date["mon"];
-        if ($today_date["mday"] < 10)
-            $birthday = "0" . $today_date["mday"];
-        else
-            $birthday = $today_date["mday"];
-        $sSql = "SELECT oxusername FROM oxuser WHERE oxbirthdate LIKE '%-".$birthmonth."-".$birthday."'";
-        $resultSet = \OxidEsales\Eshop\Core\DatabaseProvider::getDb()->select($sSql);
+        $objectIDsql = "SELECT oxobjectid FROM oxobject2group WHERE oxgroupid = 'oxidbirthdaychilds'";
+        $objectIDresultSet = \OxidEsales\Eshop\Core\DatabaseProvider::getDb()->select($objectIDsql);
 
-        $allResults = $resultSet->fetchAll();
-        $results = array();
+        $allResults = $objectIDresultSet->fetchAll();
+        $objectIDresult = array();
         foreach($allResults as $row) {
-            $results[]= $row[0];
+            $objectIDresult[]= $row[0];
         };
+    echo $objectIDresult[0];
+        $usersql = "SELECT oxusername FROM oxuser WHERE oxid = 'oxidbirthdaychilds'";
 
-        return $results;
+
     }
 }
